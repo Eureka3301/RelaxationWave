27e125229013f266147c4b7fdf8f26633593d061
Rodion Lukashov <32921341+Eureka3301@users.noreply.github.com>
2025-04-20 01:31:11 +0300

    Add files via upload

--- mech_spa.py
+++ mech_spa.py
@@ -10,6 +10,8 @@ def prnt(s=''):
         print(s)
     print('='*os.get_terminal_size()[0])
 
+g = 9.81 # N/kg
+
 class specimen():
     def __init__(self, filename, from_dump = False):
         self.filename = filename
@@ -21,19 +23,40 @@ class specimen():
         trig = float(param['trig'])
 
 
-        newcols = {'Source':'Time/s', 'CH1':'CH1/V', 'CH2':'CH2/V'}
-        self.df = pd.read_csv(self.filename, skiprows=[1], usecols=[0,1,2]).rename(columns=newcols)
+        newcols1 = {'Source':'Time/s', 'CH1':'CH1/V'}
+        self.df1 = pd.read_csv(self.filename, skiprows=[1], usecols=[0,1]).rename(columns=newcols1)
 
-        self.df['Time/s'] -= self.df.iloc[0]['Time/s']
+        newcols2 = {'Source':'Time/s', 'CH2':'CH2/V'}
+        self.df2 = pd.read_csv(self.filename, skiprows=[1], usecols=[0,2]).rename(columns=newcols2)
 
-        self.df['CH1/V'] -= self.df[self.df['Time/s'] < trig]['CH1/V'].mean()
-        self.df['CH2/V'] -= self.df[self.df['Time/s'] < trig]['CH2/V'].mean()
+        self.df1['Time/s'] -= self.df1.iloc[0]['Time/s']
+        self.df2['Time/s'] -= self.df2.iloc[0]['Time/s']
 
-        self.df['CH1/V'] = self.df['CH1/V'].rolling(50).mean()
-        self.df['CH2/V'] = self.df['CH2/V'].rolling(50).mean()
+        self.df1['CH1/V'] -= self.df1[self.df1['Time/s'] < trig]['CH1/V'].mean()
+        self.df2['CH2/V'] -= self.df2[self.df2['Time/s'] < trig]['CH2/V'].mean()
 
-        self.df['CH2/V'].shift(param['shft'])
+        self.df1['CH1/V'] = self.df1['CH1/V'].rolling(50).mean()
+        self.df2['CH2/V'] = self.df2['CH2/V'].rolling(50).mean()
 
+        self.df2['Time/s'] += float(param['shft'])
+
+
+        cut1, cut2 = map(float, param['cut'].split(','))
+
+        self.df1.drop(self.df1[ (self.df1['Time/s'] < cut1) | (self.df1['Time/s'] > cut2) ].index, inplace=True)
+        self.df2.drop(self.df2[ (self.df2['Time/s'] < cut1) | (self.df2['Time/s'] > cut2) ].index, inplace=True)
+
+        self.df1['Time/s'] -= self.df1.iloc[0]['Time/s']
+        self.df2['Time/s'] -= self.df2.iloc[0]['Time/s']
+
+        self.df1['CH1/V'] -= float(param['zero'])
+        self.df2['CH2/V'] -= float(param['zero'])
+
+        self.df1['CH1/MPa'] = self.df1['CH1/V'] * float(param['k']) * g / float(param['S']) / 1e+6
+        self.df2['CH2/MPa'] = self.df2['CH2/V'] * float(param['k']) * g / float(param['S']) / 1e+6
+
+        self.df1.reset_index(inplace=True)
+        self.df2.reset_index(inplace=True)
 
         # if from_dump:
         #     with open('dump.pickle', 'rb') as dumpfile:
@@ -55,20 +78,29 @@ relaxed_specimens = []
 for filename in filenames:
     relaxed_specimens.append(specimen(filename=filename))
 
-    prnt()
-    print(relaxed_specimens[-1].df.head())
-
 for i in range(3):
     plt.title(filenames[i])
+
+    plt.hlines(0, relaxed_specimens[i].df1.iloc[0]['Time/s'], relaxed_specimens[i].df1.iloc[-1]['Time/s'], color='r')
+
     sns.lineplot(
-        data=relaxed_specimens[i].df,
+        data=relaxed_specimens[i].df1,
         x = 'Time/s',
-        y = 'CH1/V',         
+        y = 'CH1/MPa',
+        label = 'CH1',         
                  )
     sns.lineplot(
-        data=relaxed_specimens[i].df,
+        data=relaxed_specimens[i].df2,
         x = 'Time/s',
-        y = 'CH2/V',         
+        y = 'CH2/MPa',
+        label = 'CH2',       
                  )
     
-    plt.show()
+   # plt.show()
+
+for i in range(3):
+    xldf = pd.DataFrame()
+    xldf['Time/s'] = relaxed_specimens[i].df1['Time/s']
+    xldf['CH1/MPa'] = relaxed_specimens[i].df1['CH1/MPa']
+    xldf['CH2/MPa'] = relaxed_specimens[i].df2['CH2/MPa']
+    xldf.to_excel(f"{filenames[i][:-4]}.xlsx") 
